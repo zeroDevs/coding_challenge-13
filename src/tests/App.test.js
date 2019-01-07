31bfa91cb2570c459a17c60863671f9ae67c4ac6
Aszmel <aszmel@o2.pl>
2019-01-07 20:46:39 +0100

    add test folder, failed at mocking require.context

---
+++ src/tests/App.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import App from '../App';
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("App", () => {
+    it("renders without crashing", () => {
+      const div = document.createElement("div");
+      ReactDOM.render(<App />, div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+
+    test("has a valid snapshot", () => {
+      const component = renderer.create(<App />);
+      const tree = component.toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+test('should render AddExpensePage correctly', () => {
+    wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
