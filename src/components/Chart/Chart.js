14708c16894e901d6cb829bce72260f724d835b3
Glyn L <glyn_92@hotmail.com>
2019-01-10 09:48:06 +0700

    legend only on touch devices

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,39 +2,49 @@ import React from 'react';
 import { Polar } from 'react-chartjs-2';
 import './Chart.css';
 
-const Chart = ({ data }) => (
-  <div>
-    {data === undefined ? (
-      <h2>Loading...</h2>
-    ) : (
-      <Polar
-        data={data}
-        options={{
-          maintainAspectRatio: true,
-          responsive: true,
-          scale: {
-            ticks: {
-              suggestedMin: 0,
-              suggestedMax: 50,
-            },
-          },
-          legend: {
-            position: 'bottom',
-          },
-          tooltips: {
-            callbacks: {
-              title(tooltipItem, data) {
-                return data.labels[tooltipItem[0].index];
+const Chart = ({ data }) => {
+  let legend = { display: false };
+  const isTouchDevice = window.matchMedia('(hover: none)');
+  if (isTouchDevice.matches) {
+    legend = {
+      display: true,
+      position: 'bottom',
+      labels: { usePointStyle: true },
+    };
+  }
+
+  return (
+    <div className="chart-wrapper">
+      {data === undefined ? (
+        <h2>Loading...</h2>
+      ) : (
+        <Polar
+          data={data}
+          options={{
+            maintainAspectRatio: false,
+            responsive: true,
+            scale: {
+              ticks: {
+                suggestedMin: 0,
+                suggestedMax: 50,
               },
-              label(tooltipItem, data) {
-                return `  ${data.datasets[0].data[tooltipItem.index]}%`;
+            },
+            legend,
+            tooltips: {
+              callbacks: {
+                title(tooltipItem, data) {
+                  return data.labels[tooltipItem[0].index];
+                },
+                label(tooltipItem, data) {
+                  return `  ${data.datasets[0].data[tooltipItem.index]}%`;
+                },
               },
             },
-          },
-        }}
-      />
-    )}
-  </div>
-);
+          }}
+        />
+      )}
+    </div>
+  );
+};
 
 export default Chart;
